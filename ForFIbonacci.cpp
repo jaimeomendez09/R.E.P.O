ea70405e650757f31b0015b8e60fb5903c859393
jaimeomendez09 <00068623@uca.edu.sv>
2025-05-19 00:28:22 -0600

    serie fibo con for

--- ForFIbonacci.cpp
+++ ForFIbonacci.cpp
@@ -4,8 +4,33 @@ using namespace std;
 
 int main (){
 
+    int n, x=0, y=1,z=1;
+    cout << "Ingrese la cantidad de términos de la serie de Fibonacci: ";
+    cin >> n;
+
+    for (int i = 1; i <=n; i++){
+
+        z = x + y;
+
+        cout << z << " ";
+        x = y;
+        y = z;
+   
 
 
+
+
+    }
     
-    return 0;
+
+ 
+
+
+
+
+
+
+
+
+return 0;
 }
